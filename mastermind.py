8e27594d7484b8b674265b5def9bf187db675884
jmitchel020 <jmitchel020@student.wethinkcode.co.za>
2021-11-09 13:43:28 +0200

    mastermind3

--- mastermind.py
+++ mastermind.py
@@ -1,22 +1,15 @@
 import random
 
-code = [0, 0, 0, 0]
-correct_digits_and_position = 0
-correct_digits_only = 0
-correct = False
-
 
 def create_code():
     """Function that creates the 4 digit code, using random digits from 1 to 8"""
-
-    global code
     code = [0, 0, 0, 0]
-
     for i in range(4):
         value = random.randint(1, 8) # 8 possible digits
         while value in code:
             value = random.randint(1, 8)  # 8 possible digits
         code[i] = value
+        return code
 
 
 def show_instructions():
@@ -25,28 +18,23 @@ def show_instructions():
     print('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
 
 
-def show_results():
+def show_results(correct_digits_and_position,correct_digits_only):
     """Show the results from one turn"""
-
     print('Number of correct digits in correct place:     ' + str(correct_digits_and_position))
     print('Number of correct digits not in correct place: ' + str(correct_digits_only))
 
 
-def take_turn():
+def take_turn(code):
     """Handle the logic of taking a turn, which includes:
        * get answer from user
        * check if answer is valid
        * check correctness of answer
     """
 
-    global correct_digits_and_position
-    global correct_digits_only
-
     answer = input("Input 4 digit code: ")
     while len(answer) < 4 or len(answer) > 4:
         print("Please enter exactly 4 digits.")
         answer = input("Input 4 digit code: ")
-
     correct_digits_and_position = 0
     correct_digits_only = 0
     for i in range(len(answer)):
@@ -55,43 +43,38 @@ def take_turn():
         elif int(answer[i]) in code:
             correct_digits_only += 1
 
-    show_results()
+    show_results(correct_digits_and_position,correct_digits_only)
 
 
-def show_code():
+def show_code(code):
     """Show Code that was created to user"""
 
     print('The code was: '+str(code))
 
 
-def check_correctness(turns):
+def check_correctness():
     """Checks correctness of answer and show output to user"""
-
-    global correct
-
+    turns = 0
+    correct_digits_and_position = 0
     if correct_digits_and_position == 4:
         correct = True
         print('Congratulations! You are a codebreaker!')
     else:
         print('Turns left: ' + str(12 - turns))
 
-
 def run_game():
     """Main function for running the game"""
-
-    global correct
     correct = False
 
-    create_code()
+    code = create_code()
     show_instructions()
 
     turns = 0
     while not correct and turns < 12:
-        take_turn()
+        take_turn(code)
         turns += 1
-        check_correctness(turns)
-
-    show_code()
+        check_correctness()
+    show_code(code)
 
 
 if __name__ == "__main__":
