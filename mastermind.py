2bb29a4ecb87f0e875b024f14253cf277f523387
jmitchel020 <jmitchel020@student.wethinkcode.co.za>
2021-11-10 13:38:46 +0200

    Mastermind Iteration3

--- mastermind.py
+++ mastermind.py
@@ -9,7 +9,7 @@ def create_code():
         while value in code:
             value = random.randint(1, 8)  # 8 possible digits
         code[i] = value
-        return code
+    return code
 
 
 def show_instructions():
@@ -24,19 +24,22 @@ def show_results(correct_digits_and_position,correct_digits_only):
     print('Number of correct digits not in correct place: ' + str(correct_digits_only))
 
 
-def take_turn(code):
+def answer_input():
+    answer = input("Input 4 digit code: ")
+    while len(answer) < 4 or len(answer) > 4:
+        print("Please enter exactly 4 digits.")
+        answer = input("Input 4 digit code: ")
+    return answer
+
+def take_turn(answer,code):
     """Handle the logic of taking a turn, which includes:
        * get answer from user
        * check if answer is valid
        * check correctness of answer
     """
-
-    answer = input("Input 4 digit code: ")
-    while len(answer) < 4 or len(answer) > 4:
-        print("Please enter exactly 4 digits.")
-        answer = input("Input 4 digit code: ")
     correct_digits_and_position = 0
     correct_digits_only = 0
+
     for i in range(len(answer)):
         if code[i] == int(answer[i]):
             correct_digits_and_position += 1
@@ -44,38 +47,39 @@ def take_turn(code):
             correct_digits_only += 1
 
     show_results(correct_digits_and_position,correct_digits_only)
-
+    return correct_digits_and_position,correct_digits_only
 
 def show_code(code):
     """Show Code that was created to user"""
-
+    
     print('The code was: '+str(code))
+    
 
-
-def check_correctness():
+def check_correctness(turns,correct_digits_and_position):
     """Checks correctness of answer and show output to user"""
-    turns = 0
-    correct_digits_and_position = 0
+    correct = False
     if correct_digits_and_position == 4:
+        correct = True
         print('Congratulations! You are a codebreaker!')
-        correct = False
     else:
         print('Turns left: ' + str(12 - turns))
+    return correct
 
 def run_game():
     """Main function for running the game"""
-    correct = False
 
+    correct = False
     code = create_code()
     show_instructions()
-
     turns = 0
     while not correct and turns < 12:
-        take_turn(code)
+        answer = answer_input()
+        (correct_digits_and_position, correct_digits_only) = take_turn(answer,code)
         turns += 1
-        check_correctness()
-    show_code(code)
+        correct = check_correctness(turns,correct_digits_and_position)
 
+    show_code(code)
+    
 
 if __name__ == "__main__":
     run_game()
--- test_functions.py
+++ test_functions.py
@@ -1,37 +1,81 @@
 import unittest
-import random
-from unittest.mock import patch
 from io import StringIO
+import sys
+from test_base import captured_io
+from unittest.mock import patch
+from test_base import run_unittests
+import mastermind
 
-
-    #@patch("sys.stdin", StringIO("code"))
-class TestFunctions(unittest.TestCase):    
+class TestFunctions(unittest.TestCase):
     def test_create_code(self):
-        value = random.randint(1, 8)
-        code = [0, 0, 0, 0]
-        self.assertTrue(code, value)
-    
-    
-    def test_show_instructons(self):
-        self.assertTrue('4-digit Code has been set. Digits in range 1 to 8. You have 12 turns to break it.')
-    
-    
-    def test_show_results(self):
-        correct_digits_and_position = 0
-        correct_digits_only = 0
-        self.assertTrue('Number of correct digits in correct place:     ' + str(correct_digits_and_position))
-        self.assertTrue('Number of correct digits not in correct place: ' + str(correct_digits_only))
-    
-    
-    def test_take_turn(self):
-        self.assertTrue("Input 4 digit code: ")
-    
-    def test_show_code(self):
-        code = [0,0,0,0]
-        self.assertTrue('The code was: '+str(code))
+        """
+        Tests the create_code function for:
+        Returning a random code of 4 digits and
+        for the range of the digits being 1-8.
+        """
+        output = StringIO()
+        sys.stdout = output
+        for item in range(101):
+            self.assertTrue(mastermind.create_code(),True)
+        sys.stdout = sys.__stdout__
 
-    
-    def test_check_correctness(self):
-        self.assertTrue('Congratulations! You are a codebreaker!')
-if __name__ == '__main__':
+    def test_correctness(self):
+        """
+        Tests the correctness function for:
+        If the 4 digits given from the user is the correct 4 digits chosen from
+        the create_code function.
+        If the users digits are correct then it will print
+        "Congratulations! You are a codebreaker!" and if not correct it will print
+        how many turns you have left.
+        """
+        output = StringIO()
+        sys.stdout = output
+        self.assertEqual(mastermind.check_correctness(4,5),False)
+        self.assertEqual(mastermind.check_correctness(5,7),False)
+        sys.stdout = sys.__stdout__
+
+    @patch("sys.stdin", StringIO("123\n3421\n"))
+    def test_answer_input(self):
+        """
+        Tests answer_input for:
+        If the user inputs more or less than 4 digits then
+        it will print "Please enter exactly 4 digits." and
+        it will loop again with "Input 4 digit code: " until
+        the correct digits are inputed.
+        """
+        output = StringIO()
+        sys.stdout = output
+        result = mastermind.answer_input()
+        self.assertEqual(result, "3421")
+        sys.stdout = sys.__stdout__
+
+    @patch("sys.stdin", StringIO("1234\n1342\n"))
+    def test_take_turns(self):
+        """
+        Tests the take_turns for:
+        If the correct_digits_and_position and
+        correct_digits_only are checking that the users input
+        is the same as the randomised code by correct digits,
+        correct digit position and if it is only digits.
+        If it prints out how many digits arec in orrect_digits_and_position
+        and are correct_digits_only.
+        """
+        output = StringIO()
+        sys.stdout = output
+        sys.stdout = sys.__stdout__
+        answer = ["1","2","3","4"]
+        code = [1,2,3,4]
+        result = mastermind.take_turn(answer,code)
+        compare_tuple = (4,0)
+        self.assertEqual(result,compare_tuple)
+
+
+
+
+        
+
+
+
+
+if __name__ == "__main__":
     unittest.main()
